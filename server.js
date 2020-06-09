19841580cee93ed7388cd7da91ce65216846ede0
Vladimir Gorbatov <volodya_26@mail.ru>
2020-06-10 02:46:35 +0500

    Сделал 404 и 500, настроил роуты, потаскал папочки

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+const myDir = 'static'
 
-app.use(express.static('./'));
+app.use('/', express.static(`${__dirname}/${myDir}`))
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/${myDir}/index.html`);
+})
+
+app.get('/*', (req, res) => {
+  res.sendfile(`${__dirname}/${myDir}/not-found.html`)
+})
 
 app.listen(PORT, () => {
   console.log(`Мой текст и порт: ${PORT}!`);
