c99e47f985cff65a8b8fe69e5f8cc606fce0d3f8
Vladimir Gorbatov <volodya_26@mail.ru>
2020-08-02 20:54:36 +0500

    всякое

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const myDir = 'static'
 
 app.use('/', express.static(`${__dirname}/${myDir}/`));
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/${myDir}/index.html`);
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(`${__dirname}/${myDir}/index.html`);
+// })
 
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/${myDir}/not-found.html`);
 })
 
 app.listen(PORT, () => {
-  console.log(`Приложение доступно по адресу: http://localhost:${PORT}!`);
+    console.log(`Приложение доступно по адресу: http://localhost:${PORT}!`);
 });
