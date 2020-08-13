51c6e0710227d7136fc8163f42362f455f1676ca
volodya262 <volodya_26@mail.ru>
2020-08-13 17:33:47 +0500

    отработал по query-stringify.ts

--- src/core/v-fetch/query-stringify.ts
+++ src/core/v-fetch/query-stringify.ts
@@ -1,58 +1,38 @@
-import {isObject} from "../../common/utils/common-utils";
-
-// TODO добавить спец. обработчик для Date
 // TODO скорее всего надо будет делать экранирование для спец.символов
+/**
+ * Создает строку параметров для url вида ?name=vova&age=15
+ * Поддерживает вложенные объекты
+ * @param obj
+ */
 export function queryStringify(obj: {}): string {
     if (typeof obj !== 'object') {
-        throw new Error('');
+        throw new Error(`Failed to create url params: expected object, but got ${obj}`);
     }
 
     let result = '?';
     for (const key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            if (typeof obj[key] === 'object') {
-                result += queryHelper(obj[key], key);
-            } else {
-                result += `${key}=${obj[key]}&`;
-            }
+        if (typeof obj[key] === 'object') {
+            result += queryStringifyInnerObject(obj[key], key);
+        } else {
+            result += `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}&`;
         }
     }
 
     return result.replace(/&$/, '');
 }
 
-function queryHelper(obj: {}, querykey: string) {
+function queryStringifyInnerObject(obj: {}, querykey: string) {
     let query = '';
+    const queryKeySafe = encodeURIComponent(querykey);
     for (const key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            if (typeof obj[key] === 'object') {
-                query += queryHelper(obj[key], `${querykey}[${key}]`);
-            } else {
-                query += `${querykey}[${key}]=${obj[key]}&`;
-            }
+        if (typeof obj[key] === 'object') {
+            query += queryStringifyInnerObject(obj[key], `${querykey}[${key}]`);
+        } else {
+            const safeKey = encodeURIComponent(key);
+            const safeValue = encodeURIComponent(obj[key])
+            query += `${queryKeySafe}[${safeKey}]=${safeValue}&`;
         }
     }
 
     return query;
-}
-
-/**
- * Преобразовывает объект person: {age: 15, cars: {favouriteCar: 'bmw', dailyCar: 'hyundai'}}
- * к виду person[age]=15&person[cars][favouriteCar]='bmw'&person[cars][dailyCar]='huyndai'
- * @param name
- * @param obj
- */
-function transformObjectEntriesToArrayString(name, obj): string { // большого смысла я в такой функции не вижу, копирнул из тренажера
-    const entries = Object.entries(obj);
-    return entries.reduce((acc, [key, value], index) => {
-        if (index > 0) {
-            acc += '&'
-        }
-
-        if (isObject(value)) {
-            return acc + transformObjectEntriesToArrayString(`${name}[${key}]`, value)
-        }
-
-        return acc + `${name}[${key}]=${value}`;
-    }, '')
 }
